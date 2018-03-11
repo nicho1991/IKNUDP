2144d9fc920a86173a2a569e1b02f8c718e47ce9
Max <maxbj@live.dk>
2018-03-11 13:46:52 +0100

    .

---
+++ IKNUpdatedClient/IKNClientUDP/IKNClientUDP/IKNClientUDP/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace IKNClientUDP
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string ip = args [0];
+			string command = args [1];
+			ClientData n = new ClientData (ip,command);
+		}
+	}
+
+	/// <summary>
+	/// Client data.
+	/// </summary>
+	public class ClientData{
+		const int PORT = 9000;
+		public ClientData(string ip,string command)
+		{
+			var client = new UdpClient();
+			IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ip), PORT); // endpoint where server is listening
+			client.Connect(ep);
+
+			// send data
+			string cmd = command;
+			byte[] data = new byte[1024];
+			Console.WriteLine (cmd);
+			data = Encoding.ASCII.GetBytes (cmd);
+			client.Send(data,data.Length);
+
+
+			// then receive data
+			var receivedData = client.Receive(ref ep);
+			string s = System.Text.Encoding.UTF8.GetString(receivedData, 0, receivedData.Length);
+			Console.Write("receive data from: \n" + ep.ToString()+ "\n");
+			Console.WriteLine ("\n Data recieved: \n"+s);
+
+			Console.Read();
+		}
+	}
+}
---
+++ IKNUpdatedServer/IKNServerUDP/IKNServerUDP/IKNServerUDP/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+
+
+namespace IKNServerUDP
+{
+	
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			ServerData n = new ServerData();
+
+		}
+	}
+
+	/// <summary>
+	/// Does all the server stuff
+	/// </summary>
+	public class ServerData{
+		const int PORT = 9000;
+		string UPath =  @"/proc/loadavg";
+		string LPath = @"/proc/uptime";
+		public ServerData()
+		{
+
+			while (true)
+			{
+
+				UdpClient udpServer = new UdpClient(PORT);
+				FileStream fileU = new FileStream (UPath, FileMode.Open, FileAccess.Read);
+				FileStream fileL = new FileStream (LPath, FileMode.Open, FileAccess.Read);
+
+				byte[] data = new byte[1024];
+				Console.WriteLine("Started " +"Waiting for command ");
+
+				//receive
+				var remoteEP = new IPEndPoint(IPAddress.Any, PORT); 
+				var cmdR = udpServer.Receive(ref remoteEP); //receivng command
+				var asciiCmd = AsciiConvert (cmdR);
+				//string System.Text.Encoding.Default.GetString();
+				Console.WriteLine("Received: " + asciiCmd);
+
+				switch (asciiCmd) {
+					case "U":
+					Console.WriteLine ("recieved U ");
+					int Uinfo = fileU.Read (data, 0, data.Length);
+					udpServer.Connect (remoteEP);
+					udpServer.Send (data, data.Length);
+					string s = System.Text.Encoding.UTF8.GetString(data, 0, data.Length);
+					Console.WriteLine ("replied: " + s);
+					udpServer.Close();
+						break;
+
+					case "u":
+					Console.WriteLine ("recieved u ");
+					int uinfo = fileU.Read (data, 0, data.Length);
+					udpServer.Connect (remoteEP);
+					udpServer.Send (data, data.Length);
+					string s1 = System.Text.Encoding.UTF8.GetString(data, 0, data.Length);
+					Console.WriteLine ("replied: " + s1);
+					udpServer.Close();
+						break;
+
+					case "L":
+					Console.WriteLine ("recieved L ");
+					int Linfo = fileL.Read (data, 0, data.Length);
+					udpServer.Connect (remoteEP);
+					udpServer.Send (data, data.Length);
+					string s2 = System.Text.Encoding.UTF8.GetString(data, 0, data.Length);
+					Console.WriteLine ("replied: " + s2);
+					udpServer.Close();
+						break;
+
+				case "l":
+					Console.WriteLine ("recieved l ");
+					int linfo = fileL.Read (data, 0, data.Length);
+					udpServer.Connect (remoteEP);
+					udpServer.Send (data, data.Length);
+					string s3 = System.Text.Encoding.UTF8.GetString(data, 0, data.Length);
+					Console.WriteLine ("replied: " + s3);
+					udpServer.Close();
+						break;
+
+					default:
+					string errorcmd = "Error! command not found";
+					data = Encoding.ASCII.GetBytes (errorcmd);
+
+					break;
+
+				}
+
+			}
+		}
+		private string AsciiConvert(Byte[] ConvertThis){
+			return Encoding.ASCII.GetString (ConvertThis);
+		}
+	}
+}
